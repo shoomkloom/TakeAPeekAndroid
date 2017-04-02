ee1444c24aff41f656d03ea7ef185fe1909fb62d
TakeAPeek Dev <takeapeekdev@gmail.com>
2017-04-02 22:40:48 +0300

    Adding default exception handling

--- TakeAPeekAndroid/app/src/main/java/com/takeapeek/common/TAPApplication.java
+++ TakeAPeekAndroid/app/src/main/java/com/takeapeek/common/TAPApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
+import com.takeapeek.R;
 
 
 public class TAPApplication extends Application
@@ -18,7 +19,40 @@ public class TAPApplication extends Application
     {
         super.onCreate();
 
+        // Setup handler for uncaught exceptions.
+        Thread.setDefaultUncaughtExceptionHandler (new Thread.UncaughtExceptionHandler()
+        {
+            @Override
+            public void uncaughtException (Thread thread, Throwable e)
+            {
+                handleUncaughtException (thread, e);
+            }
+        });
+
         FacebookSdk.sdkInitialize(getApplicationContext());
         AppEventsLogger.activateApp(this);
+    }
+
+    public void handleUncaughtException (Thread thread, Throwable e)
+    {
+        e.printStackTrace(); // not all Android versions will print the stack trace automatically
+
+        String threadName = thread.getName();
+        String message = null;
+
+        switch(threadName)
+        {
+            case "openCamera":
+                message = getString(R.string.error_open_camera) + "\n" + e.getMessage();
+                break;
+
+            default:
+                message = getString(R.string.error_unknown) + "\n" + e.getMessage();
+                break;
+        }
+
+        Helper.ShowCenteredToast(getApplicationContext(), message);
+
+        System.exit(1); // kill off the crashed app
     }
 }
